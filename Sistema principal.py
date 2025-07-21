dde4252c75d5b54d17439783927ab91f038fe205
Sebasgc00 <sebastian.gallo@udea.edu.co>
2025-07-20 22:47:24 -0500

    Sistema principal

---
+++ Sistema principal.py
@@ -0,0 +1,5 @@
+# Sistema principal
+"""Este sistema estara encargado de gestionar el mantenimiento de aeronaves y coordinar tecnicos y hangares para dicha tarea""" 
+
+# Extensiones
+import random as rnd
